4fd589a603bc763519d67a058c0eb7c978056f99
Donovan McGuire <donovanjmcguire@gmail.com>
2021-03-16 20:20:53 -0500

    Create newControls.js

---
+++ forms/newControls/newControls.js
@@ -0,0 +1,22 @@
+//Font weight and size changed
+
+Checkbox1.onchange=function(){
+  NSB.MsgBox("Fun Message")
+}
+
+// Changed color and set position to fixed
+Switch1.onchange=function(){
+  NSB.MsgBox( "New Value is " + Switch1.value)
+}
+
+//Changed it to pill style and changed the popStyle to tooltip
+Navs1.onclick=function(){
+  NSB.MsgBox('This will take you to an new tab...once I make it')
+}
+
+//Changed the color and background color
+
+
+Jumbotron1.onmousedown=function(){
+  NSB.MsgBox("Hello There")
+}
